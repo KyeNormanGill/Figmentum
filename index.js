e86399f68b5572912d68a3e63621b8dfaa6ae7c3
KyeNormanGill <kyenormangill0@gmail.com>
2018-02-21 19:00:34 +1100

    Memory leak testing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Figmentum = require('./classes/client.js');
-const client = new Figmentum();
+const client = new Figmentum({ messageSweepInterval: 10000, messageCacheLifetime: 1, messageCacheMaxSize: 1 });
 const { token } = require('./config.json');
 
 client.login(token);
